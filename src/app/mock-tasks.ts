40da881bdb0da733e51ea7c6edd42b75cca4cb56
Paul Thebault <contact@paulthebault.fr>
2023-05-25 20:45:58 +0200

    User Story 1: List TODOs

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,8 @@
+import { Task } from './task';
+
+export const TASKS: Task[] = [
+    { id: 0, title: "Do a cross" },
+    { id: 1, title: "Solve the first two layers" },
+    { id: 2, title: "Orientate the last layer" },
+    { id: 3, title: "Permutate the last layer" }
+];
